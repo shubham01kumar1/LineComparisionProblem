35d52102beca0663c62d2fae2a9f1c3b803e9f1a
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-08-12 20:26:33 +0530

    Adding compuatation of line's length based on a point from origin feature

--- LineComparision.java
+++ LineComparision.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class LineComparision {
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparision Computation Problem ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the X-Coordinate of point");
+       double x = sc.nextDouble();
+        System.out.println("Enter the Y-Coordinate of point");
+        double y = sc.nextDouble();
+        double length = Math.sqrt(Math.pow((x - 0), 2) + Math.pow((y - 0), 2));
+        System.out.println("Length of line is " + length);
     }
 }
